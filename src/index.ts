19d5ed0ecbd7d75a3e942a3c0136dac266f55e53
Alex Barbero <barrufat@hotmail.com>
2023-11-15 23:53:54 +0100

    Add connectToDatabase

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import morgan from "morgan";
 import express from "express";
-import app from "./app.js";
+import morgan from "morgan";
+import connectToDatabase from "./database/index.js";
+import app, { startServer } from "./server/app.js";
 import PingController from "./features/ping/controller/PingController.js";
 
+const port = process.env.PORT ?? 4000;
 const pingController = new PingController();
+
+if (!process.env.MONGODB_URL) {
+  console.log("Missing MongoDB Connection String");
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+
+startServer(+port);
 
 app.use(express.json());
 app.use(morgan("dev"));
