97e3b6c85ea36203fc7cb53cd4a0ac372539380a
Alex Barbero <barrufat@hotmail.com>
2023-11-16 18:57:56 +0100

    Refactor entry point

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import "dotenv/config";
-import express from "express";
-import morgan from "morgan";
 import connectToDatabase from "./database/index.js";
-import app, { startServer } from "./server/app.js";
-import PingController from "./features/ping/controller/PingController.js";
+import { startServer } from "./server/app.js";
+import "./server/index.js";
 
 const port = process.env.PORT ?? 4000;
-const pingController = new PingController();
 
 if (!process.env.MONGODB_URL) {
   console.log("Missing MongoDB Connection String");
@@ -18,8 +15,3 @@ const mongoUrl = process.env.MONGODB_URL;
 await connectToDatabase(mongoUrl);
 
 startServer(+port);
-
-app.use(express.json());
-app.use(morgan("dev"));
-
-app.get("/", pingController.getPong);
